08893a22750b44aa21e5332ced3bb4e027b4ef55
Agus Rumayor <agus@rumayor.org>
2017-09-18 18:04:50 +0200

    media general

--- media/F9117G.py
+++ media/F9117G.py
@@ -36,75 +36,17 @@ print (set(table.field_names) - set(total_f))
 records = table.records
 #records = table.records[1:3]
 print len(table.records)
+for record in records:
+	print record['PLANTEL']
 exit(0)
 for record in records:
-	q='CREATE VERTEX Plantel CONTENT {'
-	q=q+'"nivel":"'+nivel+'",'
-	#Informacion general
-	for field in info_general:
-		value = record[field]
-		if type(value) == unicode:
-			value = value.replace('"', '\\"')
-		q=q+'"%s":"%s",'%(renombres[field], value)
-	q=q+'ubicacion:{'
-	for field in ubicacion:
-		value = record[field]
-                if type(value) == unicode:
-                        value = value.replace('"', '\\"')
-		q=q+'"%s":"%s",'%(renombres[field], value)
-	q=q[:-1]
-	q=q+'}, '
-	q=q+'direccion:{'
-	for field in direccion:
-                value = record[field]
-                if type(value) == unicode:
-                        value = value.replace('"', '\\"')
-                q=q+'"%s":"%s",'%(renombres[field], value)
-        q=q[:-1]
-	q=q+'},'
-	q=q+'sostenimiento:{'
-        for field in sostenimiento:
-                value = record[field]
-                if type(value) == unicode:
-                        value = value.replace('"', '\\"')
-                q=q+'"%s":"%s",'%(renombres[field], value)
-        q=q[:-1]
-        q=q+'},'
-	q=q+'dependencia_normativa:{'
-        for field in dependencia_normativa:
-                value = record[field]
-                if type(value) == unicode:
-                        value = value.replace('"', '\\"')
-                q=q+'"%s":"%s",'%(renombres[field], value)
-        q=q[:-1]
-        q=q+'},'
-	q=q+'grupos:{'
-        for field in grupos:
-                value = record[field]
-                if type(value) == unicode:
-                        value = value.replace('"', '\\"')
-                q=q+'"%s":"%s",'%(renombres[field], value)
-        q=q[:-1]
-        q=q+'},'
-	q=q+'control:{'
-        for field in control:
-                value = record[field]
-                if type(value) == unicode:
-                        value = value.replace('"', '\\"')
-                q=q+'"%s":"%s",'%(renombres[field], value)
-        q=q[:-1]
-        q=q+'},'
-	q=q+'biblioteca:{'
-        for field in biblioteca:
-                value = record[field]
-                if type(value) == unicode:
-                        value = value.replace('"', '\\"')
-                q=q+'"%s":"%s",'%(renombres[field], value)
-        q=q[:-1]
-        q=q+'}'
-	q=q+'}'
-	rid_plantel = client.command(q)[0]._rid
-	#print q
+	if record['PLANTEL'] != "":
+                q='SELECT FROM Plantel WHERE clave = "%s"'%record['PLANTEL']
+                plantel_bd = client.query(q,1)
+                if len(plantel_bd) == 0:
+                        print "No hay info del plantel"
+		else:
+			rid_plantel= plantel_bd[0]._rid
 	q='CREATE VERTEX Resultados911 CONTENT {'
 	for field in fields:
 		value = record[field]
@@ -119,18 +61,11 @@ for record in records:
 			q=q+'"%s":%i,'%(field, value)
 		else:
 			print "Data error @:"+rid_plantel
-	for field in resultados911:
-                value = record[field]
-                if type(value) == unicode:
-                        value = value.replace('"', '\\"')
-			value = value.replace('\n', '')
-			value = value.replace('\r', '')
-                q=q+'"%s":"%s",'%(renombres[field], value)
         q=q[:-1]
 	q=q+'}'
 	#print q
 	rid_911 = client.command(q)[0]._rid
 	if rid_911:
-                q= 'CREATE EDGE Resultado FROM %s TO %s'%(rid_plantel, rid_911)
+                q= 'CREATE EDGE Resultado2 FROM %s TO %s'%(rid_plantel, rid_911)
                 client.command(q)
 
